90b590a9a14ad7c30812e8ffd182d8a90478d106
daisuketerashita <ekusiad.tera@icloud.com>
2021-10-17 18:36:54 +0900

    メンバー専用ページへのリダイレクト処理

--- login.php
+++ login.php
@@ -1,3 +1,15 @@
+<?php
+
+if($_POST){
+    //POST情報がある時の処理
+    $host = $_SERVER['HTTP_HOST'];
+    $uri = rtrim(dirname($_SERVER['PHP_SELF']),'/\\');
+    header("Location: //$host$uri/memberonly.php");
+    exit;
+}else{
+    //GETの時の処理
+}
+?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
